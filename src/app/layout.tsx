628413bc81f8248c33bed1d6c881d56ef5f1625b
tovimx <tovimx@gmail.com>
2024-09-17 18:38:17 -0600

    feature(website): add floating sidebar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { FloatingSidebar } from "@/components/floating-sidebar";
 
 const geistSans = localFont({
     src: "./fonts/GeistVF.woff",
@@ -27,6 +28,9 @@ export default function RootLayout({
         <html lang="en">
             <body
                 className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+                <nav>
+                    <FloatingSidebar />
+                </nav>
                 {children}
             </body>
         </html>
